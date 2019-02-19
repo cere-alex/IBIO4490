42f03aa1a1285916194e01880a734f31d994c16d
cere-alex <troya_trujis@hotmail.es>
2019-02-19 04:01:08 -0500

    Se hizo el algoritmo para blending e hybrid, hallar mejores fotos

---
+++ 04-Hybrid/blending.py
@@ -0,0 +1,51 @@
+import cv2
+import matplotlib.pyplot as plt
+from math import log2
+
+Gamma = 0.5
+Dim = 3
+rest_profundidad=6
+img = cv2.imread("id.jpg")
+X = cv2.resize(img, (512, 512))
+x, y, z = X.shape
+v = [2 ** i for i in range(int(log2(x)) - 1, rest_profundidad-1, -1)]
+longitud=len(v)
+X_data = []
+L_data = []
+X_data.append(X)
+c = 0
+plt.figure(1)
+for i in v:
+    au_x = cv2.resize(X_data[c], (i, i))
+    au_x = cv2.GaussianBlur(au_x, (Dim, Dim), Gamma)
+    X_data.append(au_x)
+    au_x = cv2.resize(au_x, (i * 2, i * 2))
+    L_data.append(cv2.absdiff(X_data[c], au_x))
+    plt.subplot(2, 10, c + 1)
+    plt.imshow(X_data[c])
+    plt.axis('off')
+    plt.subplot(2, 10, c + 11)
+    plt.imshow(L_data[c])
+    plt.axis('off')
+    c += 1
+# plt.show()
+print(au_x.shape)
+L_data = L_data[::-1]
+X_data = X_data[::-1]
+
+XX_data = []
+x,y,z=X_data[0].shape
+XX_data.append(cv2.resize(X_data[0], (x*2,x*2)))
+plt.figure(2)
+for i in range(c):
+    au_x = cv2.add(XX_data[i], L_data[i])
+    au_x = cv2.resize(au_x, (2 ** (i+rest_profundidad+2), 2 ** (i+rest_profundidad+2)))
+    XX_data.append(au_x)
+    plt.subplot(1,10,i+1)
+    plt.imshow(XX_data[i])
+    plt.axis("off")
+plt.show()
+cv2.imshow('yo',XX_data[c])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+pyramid
---
+++ 04-Hybrid/hybrid.py
@@ -0,0 +1,36 @@
+from math import log2
+from PIL import Image, ImageFilter, ImageChops
+import matplotlib.pyplot as plt
+import cv2
+img = Image.open('inti2.jpg')
+img=img.rotate(-90)
+x = 512
+y = 512
+r = 20
+
+img_dog = img.resize((x, y))
+img_dog_gaussian = img_dog.filter(ImageFilter.GaussianBlur(radius=r/1.5))
+img = Image.open('id.jpg')
+img=img.rotate(-90)
+img_i = img.resize((x, y))
+img_i_gaussian = img_i.filter(ImageFilter.GaussianBlur(radius=r*1.5))
+img_i_difference = ImageChops.difference(img_i, img_i_gaussian)
+img_sum = ImageChops.add(img_dog_gaussian, img_i_difference)
+plt.figure(1)
+plt.subplot(221)
+plt.imshow(img_dog)
+plt.axis('off')
+plt.subplot(222)
+plt.imshow(img_dog_gaussian)
+plt.axis('off')
+plt.subplot(223)
+plt.imshow(img_i)
+plt.axis('off')
+plt.subplot(224)
+plt.imshow(img_i_difference)
+plt.axis('off')
+plt.figure(2)
+plt.imshow(img_sum)
+plt.axis("off")
+plt.show()
+print(log2(x))

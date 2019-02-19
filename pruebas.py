918c3f79375d424609de3676bad2914fbd2a22e1
cere-alex <troya_trujis@hotmail.es>
2019-02-19 03:59:12 -0500

    Se hizo el algoritmo para blending e hybrid, hallar mejores fotos

--- pruebas.py
+++ pruebas.py
@@ -1,7 +1,11 @@
-import os
-
-os.system("echo hello")
-a = 1
-b = [2, 3, 4, 5, 6]
-c = a * b
-print(c)
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+img=[]
+img.append( cv2.imread('inti.jpg'))
+img.append(cv2.imread('IMG_1441.JPG'))
+print(img)
+print(type(img))
+cv2.imshow('hello', img[1])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
